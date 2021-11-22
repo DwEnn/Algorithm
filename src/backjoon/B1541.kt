71c82519db66b3b525a0f5e9b632a1201aefa5f7
DwEnn <prgohn@gmail.com>
2021-11-22 22:54:05 +0900

    Update B1541Kt.class

--- src/backjoon/B1541.kt
+++ src/backjoon/B1541.kt
@@ -4,18 +4,27 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() {
+//    BufferedReader(InputStreamReader(System.`in`)).use { br ->
+//        var result: Int? = null
+//        br.readLine().split("-").forEach {
+//            var sum = 0
+//            it.split("+").forEach {num ->
+//                sum += num.toInt()
+//            }
+//
+//            result = if (result == null) sum else result!! - sum
+//        }
+//        println(result)
+//    }
 
     BufferedReader(InputStreamReader(System.`in`)).use { br ->
-        var result: Int? = null
-        br.readLine().split("-").forEach {
-            var sum = 0
-            it.split("+").forEach {num ->
-                sum += num.toInt()
-            }
-
-            result = if (result == null) sum else result!! - sum
+        val input = br.readLine()
+        var sum = 0
+        val list = input.split("-")
+        sum = list[0].split("+").sumBy { s -> s.toInt() }
+        for (i in 1 until list.size) {
+            sum -= list[i].split("+").sumBy { s -> s.toInt() }
         }
-        println(result)
+        println(sum)
     }
-
 }
