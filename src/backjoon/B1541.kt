129b063cdcbe9e136f7153907f80eaee0b46c213
DwEnn <prgohn@gmail.com>
2022-03-13 23:30:34 +0900

    refactor: 백준 1541 풀이 수정

--- src/backjoon/B1541.kt
+++ src/backjoon/B1541.kt
@@ -4,27 +4,12 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() {
-//    BufferedReader(InputStreamReader(System.`in`)).use { br ->
-//        var result: Int? = null
-//        br.readLine().split("-").forEach {
-//            var sum = 0
-//            it.split("+").forEach {num ->
-//                sum += num.toInt()
-//            }
-//
-//            result = if (result == null) sum else result!! - sum
-//        }
-//        println(result)
-//    }
-
     BufferedReader(InputStreamReader(System.`in`)).use { br ->
-        val input = br.readLine()
-        var sum = 0
-        val list = input.split("-")
-        sum = list[0].split("+").sumBy { s -> s.toInt() }
-        for (i in 1 until list.size) {
-            sum -= list[i].split("+").sumBy { s -> s.toInt() }
-        }
-        println(sum)
+        br.readLine().split("-")
+            .map { list ->
+                list.split("+").map { it.toInt() }.sum()
+            }.reduce { acc, i ->
+                acc - i
+            }.let { println(it) }
     }
 }
