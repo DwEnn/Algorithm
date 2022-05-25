868e49e935d94bf3b445de1861ab5988d9b6a46f
DwEnn <prgohn@gmail.com>
2022-05-25 12:53:50 +0900

    백준 2644 다른 풀이 추가

--- src/backjoon/B2644.kt
+++ src/backjoon/B2644.kt
@@ -2,6 +2,7 @@ package backjoon
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.LinkedList
 
 private var N = 0
 private lateinit var visited: Array<Boolean>
@@ -12,8 +13,8 @@ fun main() {
         N = br.readLine().toInt()
         val find = br.readLine().split(" ").map { it.toInt() }
 
-        visited = Array(N +1) { false }
-        graph = Array(N +1) { IntArray(N +1) }
+        visited = Array(N + 1) { false }
+        graph = Array(N + 1) { IntArray(N + 1) }
 
         repeat(br.readLine().toInt()) {
             val nums = br.readLine().split(" ").map { it.toInt() }
@@ -48,7 +49,53 @@ private fun dfs(node: Int, find: Int, l: Int) {
             return
 
         if (!visited[i] && graph[node][i] == 1)
-            dfs(i, find, l+1)
-
+            dfs(i, find, l + 1)
     }
 }
+
+// private lateinit var graph: MutableList<MutableList<Int>>
+//
+// fun main() {
+//    BufferedReader(InputStreamReader(System.`in`)).use { br ->
+//        val N = br.readLine().toInt()
+//        val find = br.readLine().split(" ").map { it.toInt() }
+//        val m = br.readLine().toInt()
+//
+//        graph = MutableList(N + 1) { mutableListOf() }
+//        repeat(m) {
+//            val line = br.readLine().split(" ").map { it.toInt() }
+//            graph[line[0]].add(line[1])
+//            graph[line[1]].add(line[0])
+//        }
+//
+//        println(search(find[0], find[1]))
+//    }
+// }
+//
+// private fun search(start: Int, find: Int): Int {
+//    val queue = LinkedList<Int>()
+//    queue.add(start)
+//    val visited = BooleanArray(graph.size)
+//
+//    var level = -1
+//    var found = false
+//    while (!queue.isEmpty() && !found) {
+//        val size = queue.size
+//        repeat(size) {
+//            val curr = queue.poll()
+//            if (curr == find) {
+//                found = true
+//                return@repeat
+//            }
+//            for (i in graph[curr]) {
+//                if (!visited[i]) {
+//                    visited[i] = true
+//                    queue.add(i)
+//                }
+//            }
+//        }
+//        level++
+//    }
+//
+//    return if (found) level else -1
+// }
